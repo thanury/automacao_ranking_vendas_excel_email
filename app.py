f683d1d5a4b94bfb695b5c02194316b288623eb0
Thanury Tanisue <thanurytanisue@gmail.com>
2026-05-03 11:10:41 -0300

    inicializando o projeto

---
+++ app.py
@@ -0,0 +1,6 @@
+# Passo a passo do projeto
+# Passo 1: Pegar cada base de dados
+# Passo 2: Para cada base de dados
+    # calcular faturamento total (somar todos os valores da coluna de vendas)
+# Criar um rank com o faturamento total de todas as lojas
+# Enviar um email com esse ranking para a diretoria
